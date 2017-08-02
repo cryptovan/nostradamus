f8fa196ec20ebfbf5add4a8546d9dbf9c00cabdd
Eric Muyser <eric@muyser.com>
2017-08-02 00:31:11 -0700

    Clean up layout

--- src/web/containers/home/index.js
+++ src/web/containers/home/index.js
@@ -1,20 +1,31 @@
-// @flow
-
 import React from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import { WhiteSpace, Button } from 'antd-mobile'
 import { Link } from 'react-router-dom'
+import { Layout, Menu, Breadcrumb, Icon } from 'antd'
 
 import BasicLayout from '../../components/basic-layout'
 
 import './index.css'
 
+const { SubMenu } = Menu
+const { Header, Content, Sider } = Layout
+
+
 const Container = (props) => (
-    <BasicLayout className="Page" style={{ background: '#fff', padding: '30px', textAlign: 'center' }}>
-        <br />
-        <br />
-        <Link to="/">Home</Link><br />
+    <BasicLayout className="Page">
+        <Breadcrumb style={{ margin: '12px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
+            Content
+
+            <br />
+            <br />
+            <Link to="/">Home</Link><br />
+        </Content>
     </BasicLayout>
 )
 
