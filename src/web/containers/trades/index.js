df38fdc14bc9f96206a5a9c26247a356fb20b825
Eric Muyser <eric@muyser.com>
2017-08-03 00:14:25 -0700

    Init trade view. Integrate stockcharts

--- src/web/containers/trades/index.js
+++ src/web/containers/trades/index.js
@@ -9,24 +9,41 @@ import { WhiteSpace } from 'antd-mobile'
 
 import BasicLayout from '../../components/basic-layout'
 
+import Chart from './Chart';
+import { getData } from "./utils"
+
+import { TypeChooser } from "react-stockcharts/lib/helper";
+
 const { SubMenu } = Menu
 const { Header, Content, Sider } = Layout
 
-function Container() {
-    return (
-        <BasicLayout className="Page">
-            <Breadcrumb style={{ margin: '12px 0' }}>
-                <Breadcrumb.Item><Link to="/">Home</Link></Breadcrumb.Item>
-                <Breadcrumb.Item>Exchanges</Breadcrumb.Item>
-            </Breadcrumb>
-            <Content style={{ padding: 0, margin: 0 }}>
-                Trades
-            </Content>
-        </BasicLayout>
-    )
+
+class Container extends React.Component {
+    componentDidMount() {
+        getData().then(data => {
+            this.setState({ data })
+        })
+    }
+    render() {
+        if (this.state == null) {
+            return <div>Loading...</div>
+        }
+        return (
+            <BasicLayout className="Page">
+                <Breadcrumb style={{ margin: '12px 0' }}>
+                    <Breadcrumb.Item><Link to="/">Home</Link></Breadcrumb.Item>
+                    <Breadcrumb.Item>Trade View</Breadcrumb.Item>
+                </Breadcrumb>
+                <Content style={{ padding: 0, margin: 0 }}>
+                    <TypeChooser>
+                        {type => <Chart type={type} data={this.state.data} />}
+                    </TypeChooser>
+                </Content>
+            </BasicLayout>
+        )
+    }
 }
 
-Container.displayName = 'trades/Container'
 
 const mapStateToProps = state => ({
 })
